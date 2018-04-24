c9ac186bb0e555e2404014d0ec1fac29d723da10
Dénes Dinók <d.dns0098g@gmail.com>
2018-04-24 23:07:37 +0200

    feat(delete): add delete funtion, fix views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      redirect_to root_path
+      redirect_to admin_path
     else
       render 'new'
     end
@@ -15,6 +15,21 @@ class ProjectsController < ApplicationController
 
   def edit
     @project = find_project
+  end
+
+  def update
+    @project = find_project
+    if @project.update(project_params)
+      redirect_to admin_path
+    else
+      reder 'edit'
+    end
+  end
+
+  def destroy
+    @params = find_project
+    @project.destroy
+    redirect_to admin_path
   end
 
   private
