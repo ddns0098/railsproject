00f9eebf3aef1079920107900945f85dce52f57e
Dénes Dinók <d.dns0098g@gmail.com>
2018-04-25 16:08:13 +0200

    fix(security): fix endpoint access, fix uploaded image

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,16 @@
 class AdminsController < ApplicationController
+  before_action :require_login
+
   def index
     @projects = Project.all
+  end
+
+  private
+
+  def require_login
+    unless admin_signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_admin_session_path # halts request cycle
+    end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :require_login
+
   def new
     @project = Project.new
   end
@@ -33,6 +35,13 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def require_login
+    unless admin_signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_admin_session_path # halts request cycle
+    end
+  end
 
   def project_params
     params.require(:project).permit(:avatar, :title, :description)
